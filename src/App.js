a59bd422fa2a689fc6abedc0d09ed5f65770299b
TahhRahh <1234tararyan@gmail.com>
2021-08-17 13:57:46 +0100

    added Weather files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
+import React from "react";
+import Weather from "./Weather";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Pratice React App</h1>
+      <Weather />
       <footer>
         This project is coded by Tara Ryan and is 
       <a href="https://github.com/TahhRahh/pratice-react-app">
