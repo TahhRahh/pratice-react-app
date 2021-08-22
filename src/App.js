070f3fd5c943e4f4460db3987bd47ea7a0d630c5
TahhRahh <1234tararyan@gmail.com>
2021-08-22 17:55:16 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Weather from "./Weather";
 import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Pratice React App</h1>
+      <h1>Tara's Weather App</h1>
+      <div className ="container">
       <Weather />
+      </div>
+      <button>Howdy</button>
       <footer>
         This project is coded by Tara Ryan and is 
       <a href="https://github.com/TahhRahh/pratice-react-app">
